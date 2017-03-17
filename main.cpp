c2db480d68380ce665c9bb313152027eaa36300d
OmniTroid <davidskoland@gmail.com>
2017-03-17 13:14:41 +0100

    removed some newlines in main

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@
 
 int main(int argc, char *argv[])
 {
-
     AOApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     AOApplication main_app(argc, argv);
     main_app.construct_lobby();
@@ -18,7 +17,6 @@ int main(int argc, char *argv[])
     AOPacket *f_packet = new AOPacket("ALL#%");
     main_app.send_ms_packet(f_packet);
     main_app.w_lobby->show();
-
 
     return main_app.exec();
 }
