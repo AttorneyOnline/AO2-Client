5b0485965779abcdfd04b9a64849e52becacb810
Cerapter <cerap@protonmail.com>
2018-07-26 14:41:59 +0200

    Renamed window title.

--- lobby.cpp
+++ lobby.cpp
@@ -12,7 +12,7 @@ Lobby::Lobby(AOApplication *p_ao_app) : QMainWindow()
 {
   ao_app = p_ao_app;
 
-  this->setWindowTitle("Attorney Online 2");
+  this->setWindowTitle("Attorney Online 2 -- Case Café Custom Client");
 
   ui_background = new AOImage(this, ao_app);
   ui_public_servers = new AOButton(this, ao_app);
