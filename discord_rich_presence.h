e7cf1d7735aaa32adc0dc891ecaaff2bafe2a422
Cerapter <cerap@protonmail.com>
2018-08-07 20:26:16 +0200

    Discord Rich Presence updated.

--- discord_rich_presence.h
+++ discord_rich_presence.h
@@ -9,7 +9,7 @@ namespace AttorneyOnline {
 class Discord
 {
 private:
-  const char* APPLICATION_ID = "399779271737868288";
+  const char* APPLICATION_ID = "474362730397302823";
   std::string server_name, server_id;
   int64_t timestamp;
 public:
