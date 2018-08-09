f9fd9a789af5587de40ee131ce709791ca81253b
Cerapter <cerap@protonmail.com>
2018-08-09 16:36:56 +0200

    Limit mod call reason limit to 100 on serverside, too.

--- server/aoprotocol.py
+++ server/aoprotocol.py
@@ -603,7 +603,7 @@ class AOProtocol(asyncio.Protocol):
         else:
             self.server.send_all_cmd_pred('ZZ', '[{}] {} ({}) in {} ({}) with reason: {}'
                                         .format(current_time, self.client.get_char_name(), self.client.get_ip(), self.client.area.name,
-                                                self.client.area.id, args[0]), pred=lambda c: c.is_mod)
+                                                self.client.area.id, args[0][:100]), pred=lambda c: c.is_mod)
             self.client.set_mod_call_delay()
             logger.log_server('[{}][{}]{} called a moderator: {}.'.format(self.client.get_ip(), self.client.area.id, self.client.get_char_name(), args[0]))
 
