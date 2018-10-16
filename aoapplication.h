4858904d11bc9b350cf461d8455cf369b89477da
iamgoofball <iamgoofball@gmail.com>
2018-10-15 23:56:19 -0700

    forgot to bump the version number

--- aoapplication.h
+++ aoapplication.h
@@ -226,7 +226,7 @@ public:
 private:
   const int RELEASE = 2;
   const int MAJOR_VERSION = 5;
-  const int MINOR_VERSION = 1;
+  const int MINOR_VERSION = 2;
 
   QString current_theme = "default";
 
