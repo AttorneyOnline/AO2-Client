5b9bf086cfc83da38a319dd44befb544160096e7
David Skoland <davidskoland@gmail.com>
2019-01-03 00:09:24 +0100

    adjusted macos post build script

--- scripts/macos_post_build.sh
+++ scripts/macos_post_build.sh
@@ -6,8 +6,8 @@ cd ..
  
 mkdir $DST_FOLDER
  
-cp libbass.dylib $DST_FOLDER
-cp libbassopus.dylib $DST_FOLDER
+cp ./lib/libbass.dylib $DST_FOLDER
+cp ./lib/libbassopus.dylib $DST_FOLDER
  
 install_name_tool -id @executable_path/../Frameworks/libbass.dylib $DST_FOLDER/libbass.dylib
  
