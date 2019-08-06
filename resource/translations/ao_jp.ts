41dd29fbb0b5fdac3ca9169558b46972ae9ee5f2
stonedDiscord <stoned@derpymail.org>
2019-08-06 21:11:21 +0200

    ja_JP to be precise

--- resource/translations/ao_jp.ts
+++ resource/translations/ao_jp.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="jp_JP">
+<TS version="2.1" language="ja_JP">
 <context>
     <name>AOApplication</name>
     <message>
