AttorneyOnline/AO2-Client

commit 530721f2d0c4903331958c0c1fa64338f794f5ff

frame count check 2 electric boogaloo
re-fix the same bug oh joy

src/aocharmovie.cpp
@@ -178,8 +178,13 @@ void AOCharMovie::movie_ticker()
   // imagine if QT had sane stuff like "mirror on QMovie" or "resize the image on QT" or "interface with the current QMovie image" or anything else
 
   this->play_frame_sfx();
-  if(!apng){
+  if (m_movie->frameCount() == 0) {
+    //qDebug() << "Frame count was checked again, and it's still 0";
+    return;
+  }
+  else if(!apng) {
       ticker->start(m_movie->nextFrameDelay());
+  }
   }
 
 }

commit 3b4ea553665bd6c354012a937ca07f214793067a

Fix syntax error introduced in 530721f

src/aocharmovie.cpp
@@ -178,15 +178,15 @@ void AOCharMovie::movie_ticker()
   // imagine if QT had sane stuff like "mirror on QMovie" or "resize the image on QT" or "interface with the current QMovie image" or anything else
 
   this->play_frame_sfx();
-  if (m_movie->frameCount() == 0) {
-    //qDebug() << "Frame count was checked again, and it's still 0";
+
+  if (m_movie->frameCount() == 0)
+  {
     return;
   }
-  else if(!apng) {
-      ticker->start(m_movie->nextFrameDelay());
+  else if (!apng)
+  {
+    ticker->start(m_movie->nextFrameDelay());
   }
-  }
-
 }
 
 void AOCharMovie::LoadImageWithStupidMethodForFlipSupport(QImage image)