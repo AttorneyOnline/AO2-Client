78153bcf92f13584d30f2019f0f267ad07b2eae3
cents02 <46689260+cents02@users.noreply.github.com>
2020-04-05 23:13:07 +0300

    This got lost during merge

--- src/aocharmovie.cpp
+++ src/aocharmovie.cpp
@@ -27,6 +27,8 @@ void AOCharMovie::play(QString p_char, QString p_emote, QString emote_prefix)
       ao_app->get_character_path(p_char, emote_prefix + p_emote + ".png");
   QString apng_path =
       ao_app->get_character_path(p_char, emote_prefix + p_emote + ".apng");
+   QString alt_path_still = 
+      ao_app->get_character_path(p_char, p_emote + ".png");  
 
   QString placeholder_path = ao_app->get_theme_path("placeholder.gif");
   QString placeholder_default_path =
