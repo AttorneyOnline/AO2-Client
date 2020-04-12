d5354d9cf2ff6a01673e1d8ac656e436a7b1d975
in1tiate <32779090+in1tiate@users.noreply.github.com>
2020-04-12 17:30:22 -0500

    Update courtroom.cpp

--- src/courtroom.cpp
+++ src/courtroom.cpp
@@ -3007,7 +3007,7 @@ void Courtroom::handle_song(QStringList *p_contents)
 
 void Courtroom::handle_failed_login()
 {
-
+  modcall_player->play("./music/failed_login"); // aov memes
 }
 
 void Courtroom::handle_wtce(QString p_wtce, int variant)
