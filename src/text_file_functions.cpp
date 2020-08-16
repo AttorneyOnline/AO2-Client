f943e5e4550e0cb8a6a36151b8346500eeca7c7f
scatterflower <marisaposs@gameboyprinter.moe>
2020-08-16 10:44:52 -0500

    i misunderstood how this should work

--- src/text_file_functions.cpp
+++ src/text_file_functions.cpp
@@ -266,8 +266,6 @@ pos_size_type AOApplication::get_element_dimensions(QString p_identifier,
                                                     QString p_char)
 {
   QString char_ini_path = get_base_path() + "misc/" + get_chat(p_char) + "/" + p_file;
-  QString chatbox_ini = get_base_path() + "misc/chatbox/" + p_file;
-
   QString design_ini_path = get_theme_path(p_file);
   QString default_path = get_default_theme_path(p_file);
   QString f_result = read_design_ini(p_identifier, char_ini_path);
@@ -278,17 +276,14 @@ pos_size_type AOApplication::get_element_dimensions(QString p_identifier,
   return_value.y = 0;
   return_value.width = -1;
   return_value.height = -1;
-  if (f_result == "") {
-    f_result = read_design_ini(p_identifier, chatbox_ini);
-    if(f_result == ""){
-        f_result = read_design_ini(p_identifier, design_ini_path);
-        if (f_result == ""){
-          f_result = read_design_ini(p_identifier, default_path);
-          if (f_result == "")
-              return return_value;
-        }
-    }
-  }
+  if(f_result == ""){
+      f_result = read_design_ini(p_identifier, design_ini_path);
+      if (f_result == ""){
+        f_result = read_design_ini(p_identifier, default_path);
+        if (f_result == "")
+            return return_value;
+      }
+   }
 
   QStringList sub_line_elements = f_result.split(",");
 
