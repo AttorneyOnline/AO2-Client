bd7029d8b20da1e5cc51b33b7dc452f0c28d14f5
scatterflower <marisaposs@gameboyprinter.moe>
2020-08-22 01:31:02 -0500

    fixed

--- include/misc_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef MISC_FUNCTIONS_H
-#define MISC_FUNCTIONS_H
-
-#include <QCoreApplication>
-#include <QTime>
-
-void delay(int p_milliseconds);
-
-#endif // MISC_FUNCTIONS_H
--- src/misc_functions.cpp
+++
@@ -1,9 +0,0 @@
-#include "misc_functions.h"
-
-void delay(int p_milliseconds)
-{
-  QTime dieTime = QTime::currentTime().addMSecs(p_milliseconds);
-
-  while (QTime::currentTime() < dieTime)
-    QCoreApplication::processEvents(QEventLoop::AllEvents, 100);
-}
