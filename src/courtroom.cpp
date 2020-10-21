1614cb7ab243a96492d2fdc3504b7344adbd1ff4
in1tiate <32779090+in1tiate@users.noreply.github.com>
2020-10-21 15:49:34 -0500

    quick hotfix for chatblank and misc overrides

--- src/courtroom.cpp
+++ src/courtroom.cpp
@@ -1993,7 +1993,41 @@ void Courtroom::handle_chatmessage_2()
     customchar = m_chatmessage[CHAR_NAME];
   if (ui_vp_showname->text().trimmed().isEmpty()) // Whitespace showname
   {
-    ui_vp_chatbox->set_image("chatblank");
+    QString chatbox = ao_app->get_chat(customchar);
+    QString chatbox_path = ao_app->get_theme_path("chatblank");
+    if (chatbox != "") {
+      chatbox_path = ao_app->get_theme_path("misc/" + chatbox + "/chatblank");
+      if (!ui_vp_chatbox->set_chatbox(chatbox_path)) {
+        chatbox_path =
+            ao_app->get_base_path() + "misc/" + chatbox + "/chatblank";
+        if (!ui_vp_chatbox->set_chatbox(chatbox_path)) {
+          chatbox_path = ao_app->get_theme_path("chat");
+          if (!ui_vp_chatbox->set_chatbox(chatbox_path)) {
+            chatbox_path = ao_app->get_theme_path("chatbox");
+            if (!ui_vp_chatbox->set_chatbox(chatbox_path)) {
+              chatbox_path = ao_app->get_default_theme_path("chat");
+              if (!ui_vp_chatbox->set_chatbox(chatbox_path)) {
+                chatbox_path = ao_app->get_default_theme_path("chatbox");
+                ui_vp_chatbox->set_chatbox(chatbox_path);
+              }
+            }
+          }
+        }
+      }
+    }
+    else if (!ui_vp_chatbox->set_chatbox(chatbox_path)) {
+      chatbox_path = ao_app->get_theme_path("chat");
+      if (!ui_vp_chatbox->set_chatbox(chatbox_path)) {
+        chatbox_path = ao_app->get_theme_path("chatbox");
+        if (!ui_vp_chatbox->set_chatbox(chatbox_path)) {
+          chatbox_path = ao_app->get_default_theme_path("chat");
+          if (!ui_vp_chatbox->set_chatbox(chatbox_path)) {
+            chatbox_path = ao_app->get_default_theme_path("chatbox");
+            ui_vp_chatbox->set_chatbox(chatbox_path);
+          }
+        }
+      }
+    }
   }
   else // Aw yeah dude do some showname magic
   {
@@ -4490,10 +4524,10 @@ void Courtroom::set_text_color_dropdown()
         "c" + QString::number(c) + "_end", current_char));
     color_markdown_remove_list.append(
         ao_app->get_chat_markup("c" + QString::number(c) + "_remove",
-                                  current_char) == "1");
+                                current_char) == "1");
     color_markdown_talking_list.append(
         ao_app->get_chat_markup("c" + QString::number(c) + "_talking",
-                                  current_char) != "0");
+                                current_char) != "0");
 
     QString color_name = ao_app->get_chat_markup(
         "c" + QString::number(c) + "_name", current_char);
