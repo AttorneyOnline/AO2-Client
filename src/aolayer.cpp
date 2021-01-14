12b03d4f50898d3ed86f9723884653055f8a797e
in1tiate <radwoodward@vikings.grayson.edu>
2021-01-14 03:58:44 -0600

    don't check a default bg

--- src/aolayer.cpp
+++ src/aolayer.cpp
@@ -127,12 +127,7 @@ void BackgroundLayer::load_image(QString p_filename)
       ao_app->get_scaling(ao_app->read_design_ini("scaling", design_path));
   stretch = ao_app->read_design_ini("stretch", design_path).startsWith("true");
   qDebug() << "[BackgroundLayer] BG loaded: " << p_filename;
-  QList<QString> pathlist = {
-      ao_app->get_image_suffix(
-          ao_app->get_background_path(p_filename)), // check current bg
-      ao_app->get_image_suffix(ao_app->get_default_background_path(
-          p_filename))}; // then check "default"
-  start_playback(find_image(pathlist));
+  start_playback(ao_app->get_image_suffix(ao_app->get_background_path(p_filename));
 }
 
 void ForegroundLayer::load_image(QString p_filename, QString p_charname)
