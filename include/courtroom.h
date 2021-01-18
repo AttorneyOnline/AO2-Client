d41ec17fe72dbcf1b5277ab7b33b6374a9fb9e60
in1tiate <32779090+in1tiate@users.noreply.github.com>
2021-01-18 14:10:45 -0600

    Add a new "char_list" QTreeWidget that allows for categorizing characters (#399)
    * initial commit
    
    * use a "category" value instead
    
    * add filtering and taken indicator
    
    * hide serverlist header
    
    * set the items to disabled in place of the [X] text
    
    Co-authored-by: stonedDiscord <10584181+stonedDiscord@users.noreply.github.com>

--- include/courtroom.h
+++ include/courtroom.h
@@ -715,6 +715,9 @@ private:
 
   AOImage *ui_char_select_background;
 
+  // pretty list of characters
+  QTreeWidget *ui_char_list;
+
   // abstract widget to hold char buttons
   QWidget *ui_char_buttons;
 
@@ -908,6 +911,7 @@ private slots:
 
   void on_back_to_lobby_clicked();
 
+  void on_char_list_double_clicked(QTreeWidgetItem *p_item, int column);
   void on_char_select_left_clicked();
   void on_char_select_right_clicked();
   void on_char_search_changed();
