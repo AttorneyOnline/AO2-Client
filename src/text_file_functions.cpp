9dfca66041e4ad6e91f79387146ec8cd3fccd9b6
in1tiate <radwoodward@vikings.grayson.edu>
2021-04-24 16:47:25 -0500

    heehoo

--- src/text_file_functions.cpp
+++ src/text_file_functions.cpp
@@ -899,7 +899,7 @@ QString AOApplication::get_custom_realization(QString p_char)
 bool AOApplication::get_pos_is_judge(QString p_pos)
 {
   QStringList positions = read_design_ini("judges", get_background_path("design.ini")).split(",");
-  if (positions == {""})
+  if (positions.size() == 1 && positions[0] == "")
       return p_pos == "jud"; //Hardcoded BS only if we have no judges= defined
   for (QString str : positions)
       str = str.trimmed();
