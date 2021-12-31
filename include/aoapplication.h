e29a41b58ffdcaaa8dc7d73c7d8e7b6674f4f51a
oldmud0 <oldmud0@users.noreply.github.com>
2021-12-30 21:47:35 -0600

    Remove AO1 MS code
    You won't be missed :')

--- include/aoapplication.h
+++ include/aoapplication.h
@@ -75,7 +75,6 @@ public:
   void construct_courtroom();
   void destruct_courtroom();
 
-  void ms_packet_received(AOPacket *p_packet);
   void server_packet_received(AOPacket *p_packet);
 
   void send_server_packet(AOPacket *p_packet, bool encoded = true);
