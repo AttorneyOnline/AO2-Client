ff857eadaa778b56144179661e9b265e16c95ecb
Alex Noir <Varsash@gmail.com>
2022-02-19 16:15:43 +0300

    Merge branch 'better_css'

--- src/courtroom.cpp
+++ src/courtroom.cpp
@@ -1309,13 +1309,14 @@ void Courtroom::done_received()
 
   if (char_list.size() > 0)
   {
-    set_char_select_page();
+    //set_char_select_page();
     set_char_select();
   }
   else
   {
     update_character(m_cid);
     enter_courtroom();
+    set_courtroom_size();
   }
 
   set_mute_list();
