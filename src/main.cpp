704c279f481e01e4b4e6a774c72fe8cee72ccd0f
Crystalwarrior <Varsash@Gmail.com>
2022-05-23 21:45:16 +0300

    Disable DPI scaling for good (#769)

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,6 @@ int main(int argc, char *argv[])
 {
   qSetMessagePattern("%{type}: %{if-category}%{category}: %{endif}%{message}");
 
-#if QT_VERSION > QT_VERSION_CHECK(5, 6, 0)
-  // High-DPI support is for Qt version >=5.6.
-  // However, many Linux distros still haven't brought their stable/LTS
-  // packages up to Qt 5.6, so this is conditional.
-  AOApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-
   AOApplication main_app(argc, argv);
 
   AOApplication::addLibraryPath(AOApplication::applicationDirPath() + "/lib");
