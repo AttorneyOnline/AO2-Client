757045c0a6a709a706c7b29cb6d633603b2084c1
Alex Noir <Varsash@gmail.com>
2022-06-02 21:40:26 +0300

    Remove useless comments

--- src/aoemotepreview.cpp
+++ src/aoemotepreview.cpp
@@ -5,16 +5,11 @@ AOEmotePreview::AOEmotePreview(QWidget *parent, AOApplication *p_ao_app) : QWidg
   ao_app = p_ao_app;
 
   ui_viewport = new QWidget(this);
-//  ui_vp_background = new BackgroundLayer(ui_viewport, ao_app);
-//  ui_vp_speedlines = new SplashLayer(ui_viewport, ao_app);
   ui_vp_player_char = new CharLayer(ui_viewport, ao_app);
   ui_vp_player_char->setObjectName("ui_vp_player_char");
   ui_vp_player_char->masked = false;
-//  ui_vp_desk = new BackgroundLayer(ui_viewport, ao_app);
-//  ui_vp_desk->setObjectName("ui_vp_desk");
   ui_size_label = new QLabel(this);
   ui_size_label->setObjectName("ui_size_label");
-//  ui_size_label->setStyleSheet("QLabel { color: lime; background-color: black; }");
   setWindowFlag(Qt::WindowMinMaxButtonsHint, false);
   setWindowFlag(Qt::Tool);
   this->resize(256, 192);
@@ -24,18 +19,8 @@ void AOEmotePreview::set_widgets()
 {
   ui_viewport->resize(this->width(), this->height());
 
-//  ui_vp_background->move_and_center(0, 0);
-//  ui_vp_background->combo_resize(ui_viewport->width(), ui_viewport->height());
-
-//  ui_vp_speedlines->move_and_center(0, 0);
-//  ui_vp_speedlines->combo_resize(ui_viewport->width(), ui_viewport->height());
-
   ui_vp_player_char->move_and_center(0, 0);
   ui_vp_player_char->combo_resize(ui_viewport->width(), ui_viewport->height());
-
-  // the AO2 desk element
-//  ui_vp_desk->move_and_center(0, 0);
-//  ui_vp_desk->combo_resize(ui_viewport->width(), ui_viewport->height());
 
   ui_size_label->setText(QString::number(this->width()) + "x" + QString::number(this->height()));
 }
