dfc480f276b19860e1dafc57ef2a41e889c5cb5a
stonedDiscord <Tukz@gmx.de>
2022-06-06 19:21:44 +0200

    braces
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/courtroom.cpp
+++ src/courtroom.cpp
@@ -4644,8 +4644,9 @@ void Courtroom::on_sfx_context_menu_requested(const QPoint &pos)
                   [=] {
     QString p_path = ao_app->get_real_path(VPath("sounds/general/"));
     if (!dir_exists(p_path))
-      return;
-    QDesktopServices::openUrl(QUrl::fromLocalFile(p_path));
+_path)) {
+return;
+}
   }
   );
   menu->popup(ui_sfx_dropdown->mapToGlobal(pos));
