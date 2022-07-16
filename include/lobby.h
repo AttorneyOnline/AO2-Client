124a767e7ba0153bcf9b664b4730155dc077422f
stonedDiscord <Tukz@gmx.de>
2022-07-16 15:30:57 +0200

    chat is gone

--- include/lobby.h
+++ include/lobby.h
@@ -108,7 +108,6 @@ private slots:
   void on_server_list_clicked(QTreeWidgetItem *p_item, int column);
   void on_server_list_doubleclicked(QTreeWidgetItem *p_item, int column);
   void on_server_search_edited(QString p_text);
-  void on_chatfield_return_pressed();
   void on_server_list_context_menu_requested(const QPoint & pos );
   void remove_favorite();
 };
