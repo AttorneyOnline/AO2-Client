786a82d41d3307139431d1fefdbdb5a3742d1533
Salanto <62221668+Salanto@users.noreply.github.com>
2022-08-01 01:21:34 +0200

    Update credits.

--- src/lobby.cpp
+++ src/lobby.cpp
@@ -426,7 +426,7 @@ void Lobby::on_about_clicked()
          "OmniTroid, stonedDiscord, longbyte1, gameboyprinter, Cerapter, "
          "Crystalwarrior, Iamgoofball, in1tiate"
          "<p><b>Client development:</b><br>"
-         "Cents02, windrammer, skyedeving, TrickyLeifa"
+         "Cents02, windrammer, skyedeving, TrickyLeifa, Salanto"
          "<p><b>QA testing:</b><br>"
          "CaseyCazy, CedricDewitt, Chewable Tablets, CrazyJC, Fantos, "
          "Fury McFlurry, Geck, Gin-Gi, Jamania, Minx, Pandae, "
