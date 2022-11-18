4cd3c3108e725d44de9cbafc1999ca3d2c1dad51
Salanto <62221668+Salanto@users.noreply.github.com>
2022-11-18 01:19:23 +0100

    AO, why do you have datatypes and don't use them?

--- include/datatypes.h
+++ include/datatypes.h
@@ -3,16 +3,180 @@
 
 #include <QMap>
 #include <QString>
+#include <QVariant>
 
+namespace AttorneyOnline {
+
+template <typename T> T toEnum(const int &f_int)
+{
+  return QVariant(f_int).value<T>();
+}
+
+template <typename T> T toEnum(const QString &f_string)
+{
+  return QVariant(f_string).value<T>();
+}
+
+class DataTypes {
+  Q_GADGET
+
+public:
+  /**
+   * @brief Controls when the "desk" image is shown in the viewport.
+   *
+   * <https://github.com/AttorneyOnline/docs/blob/master/docs/development/network.md#in-character-message>
+   */
+  enum class DESKMOD {
+    CHAT = -1,
+    HIDDEN = 0,
+    SHOWN = 1,
+    HIDEPREANIM = 2,
+    SHOWPREANIM = 3,
+    HIDEPREANIM_PAIR = 4,
+    SHOWPREANIM_NOPAIR = 5
+  };
+  Q_ENUM(DESKMOD);
+
+  /**
+   * @brief Controls wether special emote effects are used.
+   *
+   * <https://github.com/AttorneyOnline/docs/blob/master/docs/development/network.md#in-character-message>
+   */
+  enum class EMOTE_MODIFIER {
+    NONE = 0,
+    PREANIM,
+    OBJECTION,
+    ZOOM = 5,
+    OBJECT_ZOOM
+  };
+  Q_ENUM(EMOTE_MODIFIER);
+
+  /**
+   * @brief Controls which shout is used.
+   *
+   * @details In typical AO fashion the custom one is bundled with the filename
+   * in the same argument. Make sure to split the & delimiter away and only pass
+   * the integer when checking for the enum.
+   *
+   * <https://github.com/AttorneyOnline/docs/blob/master/docs/development/network.md#in-character-message>
+   */
+  enum class SHOUT_MODIFIER {
+    NOTHING = 0,
+    HOLD_IT,
+    OBJECTION,
+    TAKE_THAT,
+    CUSTOM
+  };
+  Q_ENUM(SHOUT_MODIFIER);
+
+  /**
+   * @brief Controls which color a text message appears in.
+   *
+   * @details Prior to 2.8 [CITATION NEEDED!] this is the only way to change the
+   * color of your message, but it has been made largely obsolete by inline text
+   * color, though still serves some purpose.
+   *
+   * Rainbow text has been remove in 2.8 and will just appear as plain white.
+   * Sorry folks, no pride in AO.
+   *
+   */
+  enum class TEXT_COLOR {
+    WHITE = 0,
+    GREEN,
+    RED,
+    ORANGE,
+    BLUE,
+    YELLOW,
+    RAINBOW = 0
+  };
+  Q_ENUM(TEXT_COLOR);
+
+  struct IncomingMSPacket {
+    DESKMOD desk_mod;
+    QString pre_animation;
+    QString character;
+    QString emote;
+    QString side;
+    QString sfx_name;
+    EMOTE_MODIFIER emote_mod;
+    int char_id;
+    int sfx_delay;
+    SHOUT_MODIFIER shout_mod;
+    QString evidence;
+    bool flip;
+    bool realization;
+    TEXT_COLOR text_color;
+    //2.6 Network Extension.
+    QString showname;
+    int other_char_id;
+    QString other_char_name;
+    QPair<int,int> character_offset;
+    QPair<int,int> other_offset;
+    bool other_flip;
+    bool noninterruptpre;
+    //2.8 Network Extension
+    bool sfx_looping;
+    bool screenshake;
+    QVector<int> frames_realization;
+    QVector<int> frames_sfx;
+    bool additive;
+    QString effect;
+
+    enum indices {
+        DESK_MOD = 0,
+        PRE_EMOTE,
+        CHAR_NAME,
+        EMOTE,
+        MESSAGE,
+        SIDE,
+        SFX_NAME,
+        EMOTE_MOD,
+        CHAR_ID,
+        SFX_DELAY,
+        OBJECTION_MOD,
+        EVIDENCE_ID,
+        FLIP,
+        REALIZATION,
+        TEXT_COLOR,
+        SHOWNAME,
+        OTHER_CHARID,
+        OTHER_NAME,
+        OTHER_EMOTE,
+        SELF_OFFSET,
+        OTHER_OFFSET,
+        OTHER_FLIP,
+        IMMEDIATE,
+        LOOPING_SFX,
+        SCREENSHAKE,
+        FRAME_SCREENSHAKE,
+        FRAME_REALIZATION,
+        FRAME_SFX,
+        ADDITIVE,
+        EFFECTS,
+    };
+
+    IncomingMSPacket(QStringList packet){
+        Q_UNUSED(packet)
+    }
+
+    QStringList serialise() {
+        QStringList packet_content;
+        return packet_content;
+    }
+  };
+};
+
+} // namespace AttorneyOnline
+
+//@{
+/** Server Connection Shenanigans **/
 enum connection_type {
   TCP,
   WEBSOCKETS,
 };
 
 static QMap<QString, connection_type> to_connection_type = {
-    {"tcp", connection_type::TCP},
-    {"ws", connection_type::WEBSOCKETS}
-};
+    {"tcp", connection_type::TCP}, {"ws", connection_type::WEBSOCKETS}};
 
 struct server_type {
   QString name;
@@ -21,6 +185,7 @@ struct server_type {
   int port;
   connection_type socket_type;
 };
+//@}
 
 struct emote_type {
   QString comment;
@@ -43,6 +208,8 @@ struct evi_type {
   QString name;
   QString description;
   QString image;
+
+  operator QStringList() const { return {name, description, image}; }
 };
 
 struct chatmessage_type {
@@ -64,7 +231,12 @@ struct chatmessage_type {
 
 struct area_type {
   QString name;
+  int id;
+  int players;
   QString background;
+  QString status;
+  QString cm;
+  QString locked;
   bool passworded;
 };
 
@@ -119,6 +291,19 @@ enum EMOTE_MOD {
   ZOOM = 5,
   PREANIM_ZOOM = 6,
 };
+
+enum COLOR { WHITE = 0, GREEN, RED, ORANGE, BLUE, YELLOW, RAINBOW, PINK, CYAN };
+
+// This is for inline message-colouring.
+enum INLINE_COLOURS { INLINE_BLUE, INLINE_GREEN, INLINE_ORANGE, INLINE_GREY };
+
+enum ARUP_PARAM { PLAYERS = 0, STATUS, CM, LOCKED };
+
+enum HEALTH_TYPE { DEF = 1, PRO };
+
+enum WTCE_TYPE { WITNESS_TESTIMONY, CROSS_EXAMINATION, NOT_GUILTY, GUILTY };
+
+enum AUDIO_TYPE { MUSIC, SFX, BLIPS };
 
 enum MUSIC_EFFECT { FADE_IN = 1, FADE_OUT = 2, SYNC_POS = 4 };
 
