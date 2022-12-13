594bb65ffaa8665b44e2b3e0fa70127dc9776654
Salanto <62221668+Salanto@users.noreply.github.com>
2022-12-13 01:09:47 +0100

    Revert "Fuck this SDK. Fuck Discord."
    This reverts commit 1536adbf68b3d0a0eb96b73a6edd87c58306b591.

--- Attorney_Online/src/discord_rich_presence.cpp
+++ Attorney_Online/src/discord_rich_presence.cpp
@@ -4,62 +4,35 @@ namespace AttorneyOnline {
 
 AORPC::AORPC()
 {
-    // Pretty sure this is correct.
-    discord::Core* core_raw{};
-    auto result = discord::Core::Create(client_id, DiscordCreateFlags_NoRequireDiscord, &core_raw);
-    core = std::unique_ptr<discord::Core>(core_raw);
-    if(result != discord::Result::Ok) {
-        qDebug() << "DISCORD: Unable to create Discord RPC. Error code: " << static_cast<int>(result);
-    }
-    else {
-        qDebug() << "DISCORD: RPC ready!";
-    }
-
-    activity.SetName("Attorney Online 2");
-    activity.SetType(discord::ActivityType::Playing);
-    activity.SetInstance(true);
-    activity.GetAssets().SetLargeImage("ao2-logo");
-    activity.GetAssets().SetLargeText("Objection!");
-
-    connect(&rpc_beat, &QTimer::timeout, this, &AORPC::publish_rpc);
-    rpc_beat.start(RPC_HEARTBEAT);
+    discord::Result result = discord::Core::Create(APPLICATION_ID, DiscordCreateFlags_NoRequireDiscord, &core);
+    core->SetLogHook(discord::LogLevel::Debug, [](discord::LogLevel level, std::string message) {
+        qDebug() << "Discord:" << &level << " - " << &message;
+    });
+    Q_UNUSED(result)
 }
 
-void AORPC::publish_rpc()
+AttorneyOnline::AORPC::~AORPC()
 {
-    // I am not even sure if this is the right way to do it.
-    core->ActivityManager().UpdateActivity(activity, nullptr);
-    core->RunCallbacks();
-    rpc_beat.start(RPC_HEARTBEAT);
+
 }
 
-void AORPC::state_lobby()
+void AttorneyOnline::AORPC::state_lobby()
 {
-    qDebug() << "DISCORD: Setting Lobby state.";
-    activity.SetState("In Lobby");
-    activity.SetDetails("Idle");
-    publish_rpc();
+
 }
 
-void AORPC::state_server(std::string name, std::string server_id)
+void AttorneyOnline::AORPC::state_server(std::string name, std::string server_id)
 {
-    //TODO : This code is terrible and likely will not display the timestamp correctly.
-    auto timestamp = static_cast<int64_t>(std::time(nullptr));
-    activity.SetState("In a Server");
-    activity.SetDetails(name.c_str());
-    activity.GetTimestamps().SetStart(this->timestamp);
-    activity.GetSecrets().SetMatch(server_id.c_str());
-    this->server_id = server_id;
-    this->server_name = name;
-    this->timestamp = timestamp;
+    Q_UNUSED(name)
+    Q_UNUSED(server_id)
 }
 
-void AORPC::state_character(std::string name)
+void AttorneyOnline::AORPC::state_character(std::string name)
 {
     Q_UNUSED(name)
 }
 
-void AORPC::state_spectate()
+void AttorneyOnline::AORPC::state_spectate()
 {
 
 }
