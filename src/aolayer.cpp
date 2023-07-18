d45d196a68619bbde52b90c16c459ebeee8934a5
Satoru;1816 <81326989+Satoru-1816@users.noreply.github.com>
2023-07-17 21:57:13 -0300

    Update aolayer.cpp

--- src/aolayer.cpp
+++ src/aolayer.cpp
@@ -294,9 +294,12 @@ void AOLayer::start_playback(QString p_image)
     return;
   }
 
+  bool streamed = false;
+  
   if (p_image.startsWith('http') && !ao_app->asset_url.isEmpty()) {
     ao_app->net_manager->start_image_streaming(p_image);
     qDebug() << "Started image streaming: " << p_image;
+    streamed = true;
   }
 
   if (frame_loader.isRunning())
@@ -352,8 +355,10 @@ void AOLayer::start_playback(QString p_image)
   last_path = p_image;
   while (movie_frames.size() <= frame) // if we haven't loaded the frame we need yet
     frameAdded.wait(&mutex); // wait for the frame loader to add another frame, then check again
-  if (p_image.startsWith("http") && !ao_app->asset_url.isEmpty() && ao_app->net_manager->streamed_pixmap.isEmpty())
+  if (p_image.startsWith("http") && !ao_app->asset_url.isEmpty() && streamed)
     this->set_frame(ao_app->net_manager->streamed_pixmap);
+    streamed = false;
+    qDebug().nospace() << "Set streamed pixmap." 
   else
     this->set_frame(movie_frames[frame]);
 
