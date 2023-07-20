fb5eea18c630545a31e95fab0bfeefe4c030513f
Satoru;1816 <81326989+Satoru-1816@users.noreply.github.com>
2023-07-20 20:01:02 -0300

    Update networkmanager.h

--- include/networkmanager.h
+++ include/networkmanager.h
@@ -51,7 +51,7 @@ public:
   ~NetworkManager() = default;
 
   QImage streamed_image;
-  bool done;
+  QString streamed_path;
   bool streaming_successful = false;
 
   void connect_to_server(server_type p_server);
