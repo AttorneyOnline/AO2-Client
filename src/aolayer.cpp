6f660af07267648dc4063572447b3e99cd1d7214
Satoru;1816 <81326989+Satoru-1816@users.noreply.github.com>
2023-07-21 17:00:31 -0300

    Update aolayer.cpp

--- src/aolayer.cpp
+++ src/aolayer.cpp
@@ -11,8 +11,6 @@ static QThreadPool *thread_pool;
 AOLayer::AOLayer(QWidget *p_parent, AOApplication *p_ao_app) : QLabel(p_parent)
 {
   ao_app = p_ao_app;
-
-  NetworkManager* networkManager = ao_app->net_manager;
   
   // used for culling images when their max_duration is exceeded
   shfx_timer = new QTimer(this);
@@ -300,6 +298,7 @@ void AOLayer::start_playback(QString p_image)
     qDebug() << "Loading... " << p_image;
     ao_app->net_manager->start_image_streaming(p_image);
     qDebug() << "Started image streaming: " << p_image;
+    NetworkManager* networkManager = ao_app->net_manager;
     connect(networkManager, &NetworkManager::imageLoaded, this, &AOLayer::onImageLoaded);
     p_image = ao_app->get_real_path(ao_app->get_misc_path("default", "loading"));
   }
