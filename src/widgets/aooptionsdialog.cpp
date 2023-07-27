198ec2d40c7609e529047e5bd1c90780f00833d9
Satoru;1816 <81326989+Satoru-1816@users.noreply.github.com>
2023-07-26 23:51:36 -0300

    Update aooptionsdialog.cpp

--- src/widgets/aooptionsdialog.cpp
+++ src/widgets/aooptionsdialog.cpp
@@ -228,32 +228,6 @@ void AOOptionsDialog::savePressed()
   close();
 }
 
-void AOOptionsDialog::addCharacterRow(QString characterName, QString downloadLink)
-{
-  int row = ui_download_table->rowCount();
-  ui_download_table->insertRow(row);
-
-  QTableWidgetItem* characterItem = new QTableWidgetItem(characterName);
-  ui_download_table->setItem(row, 0, characterItem);
-
-  QTableWidgetItem* linkItem = new QTableWidgetItem(downloadLink);
-  ui_download_table->setItem(row, 1, linkItem);
-}
-
-void AOOptionsDialog::removeCharacterRow(QString characterName)
-{
-  for (int row = 0; row < ui_download_table->rowCount(); ++row)
-  {
-      QTableWidgetItem* item = ui_download_table->item(row, 0);
-      if (item && item->text() == characterName)
-      {
-          ui_download_table->removeRow(row);
-          break;
-      }
-  }
-}
-
-
 void AOOptionsDialog::discardPressed() { close(); }
 
 void AOOptionsDialog::buttonClicked(QAbstractButton *button)
@@ -672,6 +646,38 @@ void AOOptionsDialog::setupUI()
   });
   updateValues();
 }
+
+
+void AOOptionsDialog::addCharacterRow(QString characterName, QString downloadLink)
+{
+  ui_download_table->setRowCount(1);
+  ui_download_table->setColumnCount(1);
+  int row = ui_download_table->rowCount();
+  ui_download_table->insertRow(row);
+
+  qDebug() << "addCharacterRow: row" << row; 
+
+
+  QTableWidgetItem* characterItem = new QTableWidgetItem(characterName);
+  ui_download_table->setItem(row, 1, characterItem);
+
+  QTableWidgetItem* linkItem = new QTableWidgetItem(downloadLink);
+  ui_download_table->setItem(row, 1, linkItem);
+}
+
+void AOOptionsDialog::removeCharacterRow(QString characterName)
+{
+  for (int row = 0; row < ui_download_table->rowCount(); ++row)
+  {
+      QTableWidgetItem* item = ui_download_table->item(row, 0);
+      if (item && item->text() == characterName)
+      {
+          ui_download_table->removeRow(row);
+          break;
+      }
+  }
+}
+
 
 void AOOptionsDialog::onTimestampFormatEdited()
 {
