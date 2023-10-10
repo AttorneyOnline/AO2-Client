0218d429e67de3900d9bce6377366aa45c155a85
Satoru;1816 <81326989+Satoru-1816@users.noreply.github.com>
2023-10-10 13:20:24 -0300

    Update lobby.cpp

--- src/lobby.cpp
+++ src/lobby.cpp
@@ -47,6 +47,7 @@ void Lobby::on_tab_changed(int index)
     ui_add_server_button->setVisible(false);
     ui_edit_favorite_button->setVisible(false);
     ui_direct_connect_button->setVisible(true);
+    ui_load_demo_button->setVisible(false);
     reset_selection();
     break;
   case FAVORITES:
@@ -56,6 +57,7 @@ void Lobby::on_tab_changed(int index)
     ui_add_server_button->setVisible(true);
     ui_edit_favorite_button->setVisible(true);
     ui_direct_connect_button->setVisible(false);
+    ui_load_demo_button->setVisible(false);
     reset_selection();
     break;
   case DEMOS:
