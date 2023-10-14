ad778325f143065f71c1f566b302697b6b546845
Satoru;1816 <81326989+Satoru-1816@users.noreply.github.com>
2023-10-13 22:13:05 -0300

    Update aoapplication.cpp

--- src/aoapplication.cpp
+++ src/aoapplication.cpp
@@ -132,7 +132,7 @@ void AOApplication::server_disconnected()
         destruct_courtroom();
     } else if (msgBox.clickedButton() == btn2) {
         net_manager->connect_to_server(net_manager->last_server_chosen);
-        connect(net_manager, &net_manager::server_connected, net_manager, &net_manager::join_to_server);
+        QObject::connect(net_manager, &NetworkManager::server_connected, net_manager, &NetworkManager::join_to_server);
   }
   Options::getInstance().setServerSubTheme(QString());
   }
