4f1646c204cb126109295829fad9facb80200287
Satoru;1816 <81326989+Satoru-1816@users.noreply.github.com>
2023-10-30 14:20:28 -0300

    Update courtroom.cpp

--- src/courtroom.cpp
+++ src/courtroom.cpp
@@ -515,7 +515,7 @@ Courtroom::Courtroom(AOApplication *p_ao_app) : QMainWindow()
   connect(action_settings, &QAction::triggered, this, &Courtroom::on_settings_clicked);
   connect(action_return_lobby, &QAction::triggered, this, [this]() {
       QMessageBox::StandardButton reply;
-      reply = QMessageBox::question(this, "", "Are you sure you want to return to the lobby?",
+      reply = QMessageBox::question(this, "Exit", "Are you sure you want to return to the lobby?",
                                     QMessageBox::Yes | QMessageBox::No);
       if (reply == QMessageBox::Yes) {
           on_back_to_lobby_clicked();
