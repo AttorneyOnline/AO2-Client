24481d9183607226fb24ca07dc319c02f010ed13
Satoru;1816 <81326989+Satoru-1816@users.noreply.github.com>
2023-11-13 16:33:56 -0300

    Add a message box when downloading a character

--- src/networkmanager.cpp
+++ src/networkmanager.cpp
@@ -10,6 +10,7 @@
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <QNetworkReply>
+#include <QDesktopServices>
 
 NetworkManager::NetworkManager(AOApplication *parent) : QObject(parent)
 {
@@ -393,6 +394,18 @@ void NetworkManager::save_folder(const QByteArray& folderData, const QString& pa
     QDir dir(sanitizedFolderName);
     if (!dir.exists()) {
         dir.mkpath(".");
+        QMessageBox msgBox;
+        msgBox.setText(tr("Download has started. Do you wish to open the character's folder?");
+        QPushButton* btn1 = msgBox.addButton(tr("Accept"), QMessageBox::AcceptRole);
+        QPushButton* btn2 = msgBox.addButton(tr("Cancel"), QMessageBox::RejectRole);
+        msgBox.setDefaultButton(btn2);
+        msgBox.exec();
+
+        if (msgBox.clickedButton() == btn1) {
+          QDesktopServices::openUrl(QUrl::fromLocalFile(sanitizedFolderName));
+        } else {
+          return;
+        }
     }
 
     QRegularExpression regex("<a href=\"(.*?)\">(.*?)<\\/a>");
