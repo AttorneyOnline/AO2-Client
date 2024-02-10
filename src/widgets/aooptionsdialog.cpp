d309fff1bb53e0ffdcf41f01fbe189058fdd6baf
Salanto <62221668+Salanto@users.noreply.github.com>
2024-02-10 10:39:32 +0100

    Fix compile error

--- src/widgets/aooptionsdialog.cpp
+++ src/widgets/aooptionsdialog.cpp
@@ -216,7 +216,10 @@ void AOOptionsDialog::updateValues()
 
 void AOOptionsDialog::savePressed()
 {
-  bool l_reload_theme_required = (ui_theme_combobox->currentText() != Options::getInstance().theme()) || (ui_theme_scaling_factor_sb->value() != Options.getInstance().themeScalingFactor());
+  bool l_reload_theme_required =
+      (ui_theme_combobox->currentText() != Options::getInstance().theme()) ||
+      (ui_theme_scaling_factor_sb->value() !=
+       Options::getInstance().themeScalingFactor());
   for (const OptionEntry &entry : qAsConst(optionEntries)) {
     entry.save();
   }
