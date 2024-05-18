5055e72f01160685134184501ac5d2dc650e85e8
UnDeviato <fevola.davide@gmail.com>
2024-05-18 14:42:35 +0200

    add set_format to timer functions

--- src/aoclocklabel.cpp
+++ src/aoclocklabel.cpp
@@ -22,13 +22,15 @@ void AOClockLabel::set(qint64 msecs, bool update_text)
   {
     if (QDateTime::currentDateTime() >= m_target_time)
     {
-      this->setText("00:00:00.000");
+      QTime timezero = QTime(0, 0);
+      QString timestring = timezero.toString(time_format);
+      this->setText(timestring);
     }
     else
     {
       qint64 ms_left = QDateTime::currentDateTime().msecsTo(m_target_time);
       QTime timeleft = QTime(0, 0).addMSecs(ms_left % (1000 * 3600 * 24));
-      QString timestring = timeleft.toString("hh:mm:ss.zzz");
+      QString timestring = timeleft.toString(time_format);
       this->setText(timestring);
     }
   }
@@ -41,7 +43,9 @@ void AOClockLabel::pause()
 
 void AOClockLabel::stop()
 {
-  this->setText("00:00:00.000");
+  QTime timezero = QTime(0, 0);
+  QString timestring = timezero.toString(time_format);
+  this->setText(timestring);
   m_timer.stop();
 }
 
@@ -67,11 +71,19 @@ void AOClockLabel::timerEvent(QTimerEvent *event)
     }
     qint64 ms_left = QDateTime::currentDateTime().msecsTo(m_target_time);
     QTime timeleft = QTime(0, 0).addMSecs(ms_left % (1000 * 3600 * 24));
-    QString timestring = timeleft.toString("hh:mm:ss.zzz");
+    QString timestring = timeleft.toString(time_format);
     this->setText(timestring);
   }
   else
   {
     QWidget::timerEvent(event);
+  }
+}
+
+void AOClockLabel::set_format(QString formatting, qint64 timer_value)
+{
+  time_format = formatting;
+  if (!this->active()) {
+    this->set(timer_value, true);
   }
 }
