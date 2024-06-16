7ae9a59b59b3f5d2750d9ac2e1fd451becb5bd9f
Salanto <62221668+Salanto@users.noreply.github.com>
2024-06-16 03:07:23 +0200

    Cooking code spaghetti

--- src/widgets/playerlistwidget.cpp
+++ src/widgets/playerlistwidget.cpp
@@ -1,30 +1,98 @@
 #include "playerlistwidget.h"
 #include "aoapplication.h"
+#include "qjsonobject.h"
+#include "qnamespace.h"
 
 #include <QJsonArray>
 #include <QJsonDocument>
 #include <QJsonObject>
+#include <optional>
 
 PlayerListWidget::PlayerListWidget(AOApplication *ao_app, QWidget *parent)
     : QListWidget(parent)
     , ao_app(ao_app)
 {}
 
-void PlayerListWidget::yeet()
+void PlayerListWidget::populateList(PlayerList list)
+{
+  reset();
+  for (const auto &player : list.list())
+  {
+    addPlayerItem(player.uid, player.icon, player.text);
+  }
+}
+
+void PlayerListWidget::updatePlayerList(PlayerListUpdate update)
+{
+  switch (update.type())
+  {
+  case PlayerListUpdate::ADD:
+  {
+    addPlayerItem(update.uid(), update.icon(), update.text());
+  }
+  break;
+  case PlayerListUpdate::REMOVE:
+  {
+    if (m_items.contains(update.uid()))
+    {
+      delete m_items.value(update.uid());
+      m_items.remove(update.uid());
+    }
+    else
+    {
+      qWarning() << "Unable to remove nonexistant entry of uid" << update.uid();
+    }
+  }
+
+  break;
+  }
+}
+
+void PlayerListWidget::updatePlayerEntry(PlayerListEntryUpdate update)
+{
+  auto l_item = getItemPointerById(update.uid());
+  if (!l_item)
+  {
+    qWarning() << "Unable to edit nonexistant entry of uid" << update.uid();
+    return;
+  }
+
+  switch (update.updateType())
+  {
+  case PlayerListEntryUpdate::UPDATEICON:
+  {
+    QIcon char_icon = QIcon(ao_app->get_image_suffix(ao_app->get_character_path(update.data(), "char_icon"), true));
+    l_item.value()->setIcon(char_icon);
+  }
+  break;
+
+  case PlayerListEntryUpdate::UPDATETEXT:
+    l_item.value()->setText(update.data());
+    break;
+  }
+}
+
+void PlayerListWidget::reset()
 {
   clear();
   m_items.clear();
 }
 
-void PlayerListWidget::populateList(PlayerList list)
+void PlayerListWidget::addPlayerItem(int uid, QString icon, QString text)
 {
-  for (const auto &player : list.list())
+  QIcon char_icon = QIcon(ao_app->get_image_suffix(ao_app->get_character_path(icon, "char_icon"), true));
+  auto l_item = new QListWidgetItem(char_icon, text, this);
+  l_item->setData(Qt::UserRole + 1, uid);
+  m_items.insert(uid, l_item);
+}
+
+std::optional<QListWidgetItem *> PlayerListWidget::getItemPointerById(int uid)
+{
+  if (!m_items.contains(uid))
   {
-    QIcon char_icon = QIcon(ao_app->get_image_suffix(ao_app->get_character_path(player.icon, "char_icon"), true));
-    QListWidgetItem *l_item = new QListWidgetItem(char_icon, player.text, this, Qt::UserRole);
-    l_item->setData(Qt::UserRole, player.uid);
-    m_items.insert(player.uid, l_item);
+    return std::nullopt;
   }
+  return m_items.value(uid);
 }
 
 PlayerList::PlayerList(QByteArray f_data)
@@ -49,4 +117,39 @@ PlayerList::PlayerList(QByteArray f_data)
 
     m_initial_list.append(player);
   }
+}
+
+PlayerListUpdate::PlayerListUpdate(QByteArray f_data)
+{
+  QJsonParseError error;
+  QJsonDocument l_json = QJsonDocument::fromJson(f_data, &error);
+
+  if (error.error != QJsonParseError::NoError)
+  {
+    qWarning() << "Invalid or malformed initial playerlist received." << Qt::endl << "ErrorString" << error.errorString();
+    return;
+  }
+
+  QJsonObject data = l_json.object();
+  m_uid = data["uid"].toInt();
+  m_icon = data["icon"].toString();
+  m_text = data["text"].toString();
+  m_update_type = data["type"].toVariant().value<UpdateType>();
+}
+
+PlayerListEntryUpdate::PlayerListEntryUpdate(QByteArray f_data)
+{
+  QJsonParseError error;
+  QJsonDocument l_json = QJsonDocument::fromJson(f_data, &error);
+
+  if (error.error != QJsonParseError::NoError)
+  {
+    qWarning() << "Invalid or malformed initial playerlist received." << Qt::endl << "ErrorString" << error.errorString();
+    return;
+  }
+
+  QJsonObject data = l_json.object();
+  m_uid = data["uid"].toInt();
+  m_data = data["data"].toString();
+  m_update_type = data["type"].toVariant().value<UpdateType>();
 }
--- src/widgets/playerlistwidget.h
+++ src/widgets/playerlistwidget.h
@@ -3,20 +3,27 @@
 #include <QListWidget>
 
 class PlayerList;
+class PlayerListUpdate;
+class PlayerListEntryUpdate;
 class AOApplication;
 
 class PlayerListWidget : public QListWidget
 {
 public:
   PlayerListWidget(AOApplication *ao_app, QWidget *parent);
-  void yeet();
 
 public Q_SLOTS:
   void populateList(PlayerList list);
+  void updatePlayerList(PlayerListUpdate update);
+  void updatePlayerEntry(PlayerListEntryUpdate update);
 
 private:
   AOApplication *ao_app;
   QMap<int, QListWidgetItem *> m_items;
+
+  void reset();
+  void addPlayerItem(int uid, QString icon, QString text);
+  std::optional<QListWidgetItem *> getItemPointerById(int uid);
 };
 
 class PlayerList
@@ -42,12 +49,45 @@ class PlayerListUpdate
 {
 public:
   PlayerListUpdate(QByteArray f_data);
-  ~PlayerListUpdate() = default;
+  ~PlayerListUpdate();
 
-  enum Type
+  enum UpdateType
   {
     ADD,
-    REMOVE,
-    UPDATE
+    REMOVE
   };
+
+  int uid() { return m_uid; };
+  QString icon() { return m_icon; };
+  QString text() { return m_text; };
+  UpdateType type() { return m_update_type; };
+
+private:
+  UpdateType m_update_type;
+  int m_uid;
+  QString m_icon;
+  QString m_text;
 };
+Q_DECLARE_METATYPE(PlayerListUpdate::UpdateType)
+
+class PlayerListEntryUpdate
+{
+public:
+  PlayerListEntryUpdate(QByteArray f_data);
+  ~PlayerListEntryUpdate() = default;
+
+  enum UpdateType
+  {
+    UPDATEICON,
+    UPDATETEXT
+  };
+
+  int uid() const { return m_uid; };
+  QString data() const { return m_data; };
+  UpdateType updateType() const { return m_update_type; };
+
+  int m_uid;
+  QString m_data;
+  UpdateType m_update_type;
+};
+Q_DECLARE_METATYPE(PlayerListEntryUpdate::UpdateType)
