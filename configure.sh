10fd2e556537898541c2199ec065aee32b91bdfb
David Skoland <davidskoland@gmail.com>
2024-06-30 23:27:56 +0200

    Add preliminary configure script

---
+++ configure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# Check platform
+# Check that we have curl in path
+# Other sanity checks
+# Install QtApng
+# Install Discord RPC
+# Install BASS
+# Install BASS Opus
+# Install BASS Midi
