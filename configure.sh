8b170575f58aec75bd1dfe6656ac6d0fffd54a76
David Skoland <davidskoland@gmail.com>
2024-07-11 17:36:59 +0200

    Implement check_command

--- configure.sh
+++ configure.sh
@@ -17,12 +17,30 @@ detect_platform() {
     echo "${platform}"
 }
 
+check_command() {
+    COMMAND=$1
+    if ! command -v "${COMMAND}" &> /dev/null; then
+        echo "${COMMAND} not found. Aborting."
+        exit 1
+    fi
+}
+
 print_help() {
     echo "Usage: $0 [options]"
     echo "Options:"
     echo "  -h, --help: Print this help message"
 }
 
+install_discordrcp_windows() {
+    curl -L https://github.com/discordapp/discord-rpc/releases/download/v3.4.0/discord-rpc-win.zip -o discord_rpc.zip
+    unzip discord_rpc.zip
+    cp ./discord-rpc/win64-dynamic/lib/discord-rpc.lib ./lib/
+    cp ./discord-rpc/win64-dynamic/bin/discord-rpc.dll ./bin/
+    cp ./discord-rpc/win64-dynamic/include/discord*.h ./lib/
+}
+
+check_command cmake
+check_command curl
 
 platform=$(detect_platform)
 echo "Detected platform: ${platform}"
