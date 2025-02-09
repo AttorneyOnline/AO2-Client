37fdf1ee6bce968c07437d91aadf7e761a6ff403
OrioleNix <163466443+OrioleNix@users.noreply.github.com>
2025-02-09 17:40:49 -0500

    Update appimage_integrator.sh
    Removed the redundant 'u' in the move command, removed all direct versioning calls and replaced them with abstracts.

--- scripts/appimage_integrator.sh
+++ scripts/appimage_integrator.sh
@@ -8,16 +8,16 @@ cd "${SCRIPT_DIR}"
 
 #put into user's local library
 mkdir ~/.local/Attorney_Online
-mv -fu {.*,*} ~/.local/Attorney_Online
+mv -f {.*,*} ~/.local/Attorney_Online
 
 #add .desktop file (which should allow most DE's easy access to the program
 mkdir ~/.local/share/applications
 rm ~/.local/share/applications/'Attorney Online'.desktop
 touch ~/.local/share/applications/'Attorney Online'.desktop
 #please note that the best way forward here is to have the real logo in path, but this is a temporary logo, easily replaceable.
-printf "[Desktop Entry]\nType=Application\nName=Attorney Online\nComment=The courtroom drama simulator\nExec=$(realpath ~/.local/Attorney_Online/Attorney_Online-2.11-x86_64.AppImage)\nIcon=$(realpath ~/.local/Attorney_Online)/base/themes/AOHD/placeholder.webp\nCategories=Game;" >> ~/.local/share/applications/'Attorney Online'.desktop
+printf "[Desktop Entry]\nType=Application\nName=Attorney Online\nComment=The courtroom drama simulator\nExec=$(realpath ~/.local/Attorney_Online/Attorney_Online-*-x86_64.AppImage)\nIcon=$(realpath ~/.local/Attorney_Online)/base/themes/AOHD/placeholder.webp\nCategories=Game;" >> ~/.local/share/applications/'Attorney Online'.desktop
 
 #marking the program as executable
-chmod +x ~/.local/Attorney_Online/Attorney_Online-2.11-x86_64.AppImage
+chmod +x ~/.local/Attorney_Online/Attorney_Online-*-x86_64.AppImage
 #running the executable
-~/.local/Attorney_Online/Attorney_Online-2.11-x86_64.AppImage
+~/.local/Attorney_Online/Attorney_Online-*-x86_64.AppImage
