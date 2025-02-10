4c4445c9e5fcb118735acac76c2bc9a3c3fdad8a
in1tiate <32779090+in1tiate@users.noreply.github.com>
2025-02-09 18:10:22 -0600

    remember category expansion state when regenerating musiclist

--- src/courtroom.cpp
+++ src/courtroom.cpp
@@ -1678,6 +1678,17 @@ void Courtroom::enter_courtroom()
 // Todo: multithread this due to some servers having large as hell music list
 void Courtroom::list_music()
 {
+  // remember collapsed categories
+  QStringList collapsed_categories;
+  for (int i = 0; i < ui_music_list->topLevelItemCount(); ++i)
+  {
+    const auto pCategory = ui_music_list->topLevelItem(i);
+    if (!pCategory->isExpanded())
+    {
+      collapsed_categories.append(pCategory->text(0));
+    }
+  }
+
   ui_music_list->clear();
   //  ui_music_search->setText("");
 
@@ -1767,9 +1778,23 @@ void Courtroom::list_music()
     }
   }
 
-  ui_music_list->expandAll(); // Needs to somehow remember which categories were
-                              // expanded/collapsed if the music list didn't
-                              // change since last time
+  ui_music_list->expandAll();
+
+  // restore expanded state from before the list was reset
+  // disable animations while we do this
+  ui_music_list->setAnimated(false);
+  for (int i = 0; i < ui_music_list->topLevelItemCount(); ++i)
+  {
+    const auto pCategory = ui_music_list->topLevelItem(i);
+    if (collapsed_categories.contains(pCategory->text(0)))
+    {
+      pCategory->setExpanded(false);
+    }
+  }
+  // restore animated state
+  QString music_list_animated = ao_app->get_design_element("music_list_animated", "courtroom_design.ini");
+  ui_music_list->setAnimated(music_list_animated == "1" || music_list_animated.startsWith("true"));
+
   if (ui_music_search->text() != "")
   {
     on_music_search_edited(ui_music_search->text());
