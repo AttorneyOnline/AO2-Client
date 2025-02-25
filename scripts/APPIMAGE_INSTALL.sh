be9829578bbe8877e55a8195dfd00434c05dd991
lambdcalculus <lambdcalcullus@gmail.com>
2025-02-25 16:09:36 -0300

    include icon in linux install

--- scripts/APPIMAGE_INSTALL.sh
+++ scripts/APPIMAGE_INSTALL.sh
@@ -6,14 +6,14 @@ cd "${SCRIPT_DIR}"
 
 #add .desktop file (which should allow most DE's easy access to the program
 mkdir -p ~/.local/share/applications
-#please note that the best way forward here is to have the real logo in path, but this is a temporary logo, easily replaceable.
+
 desktop_file="
 [Desktop Entry]
 Type=Application
 Name=Attorney Online
 Comment=The courtroom drama simulator
 Exec=$(realpath $(pwd)/Attorney_Online-*-x86_64.AppImage)
-Icon=$(pwd)/base/themes/AOHD/placeholder.webp"
+Icon=$(pwd)/icon.png"
 
 echo "$desktop_file" > ~/.local/share/applications/'Attorney Online'.desktop
 
