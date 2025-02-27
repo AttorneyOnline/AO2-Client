365ad2fd59c48792e8754f9cf4c01850747d53f1
lambdcalculus <lambdcalcullus@gmail.com>
2025-02-27 11:14:28 -0300

    why would freedesktop.org do this to me

--- scripts/APPIMAGE_INSTALL.sh
+++ scripts/APPIMAGE_INSTALL.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# exit on error
+set -e
+
 # Move to script's directory
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd "${SCRIPT_DIR}"
@@ -7,13 +10,17 @@ cd "${SCRIPT_DIR}"
 #add .desktop file (which should allow most DE's easy access to the program
 mkdir -p ~/.local/share/applications
 
+# desktop files don't like spaces in the Exec field, we have to replace them with "\s"
+appimage="$(echo Attorney_Online-*-x86_64.AppImage)"
+escaped_exec="$(echo "$(pwd)" | sed 's/ /\\s/g')"/"$appimage"
+
 desktop_file="\
 [Desktop Entry]
 Type=Application
 Name=Attorney Online
 Comment=The courtroom drama simulator
 Path=$(pwd)
-Exec=$(realpath "$(pwd)"/Attorney_Online-*-x86_64.AppImage)
+Exec=\"$escaped_exec\"
 Icon=$(pwd)/icon.png"
 
 echo "$desktop_file" > ~/.local/share/applications/'Attorney Online'.desktop
