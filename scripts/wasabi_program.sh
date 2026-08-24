10ce5f59bad9ad1fa8598b627a4f4315fae19de2
David Skoland <davidskoland@gmail.com>
2026-08-24 20:15:06 +0200

    Update filename

--- scripts/wasabi_program.sh
+++ scripts/wasabi_program.sh
@@ -28,7 +28,7 @@ export ARCHIVE="Attorney_Online_${VERSION}_${ARTIFACT_SUFFIX}"
 
 ${S3_COPY} ${S3_MANIFESTS}/${MANIFEST} .
 node $(dirname $0)/update_manifest.js ${MANIFEST} ${VERSION} \
-    -f ${ARCHIVE} -e Attorney_Online.exe
+    -f ${ARCHIVE} -e "Attorney Online.exe"
 ${S3_COPY} ${ARCHIVE} ${S3_ARCHIVES}
 ${S3_COPY} ${MANIFEST} ${S3_MANIFESTS}
 
